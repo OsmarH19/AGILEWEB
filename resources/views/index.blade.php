15fd54db96183415992aa7063123ea8edc579414
OsmarH19 <115659833+OsmarH19@users.noreply.github.com>
2024-06-17 18:58:02 -0500

    Creación de Componentes
    Por Jeferson Huidobro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AGUILE</title>
+    <title>AGILE WEB</title>
 </head>
 <body>
-    <H1>COMENZAR</H1>
+    <h1>Comenzamos con la web de Angelo Bendezu</h1>
 </body>
 </html>
