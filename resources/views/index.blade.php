8cd027f83648adfa4bcf9d1c4cd7aced86e8d3ed
OsmarH19 <115659833+OsmarH19@users.noreply.github.com>
2024-06-17 19:26:33 -0500

    Instalación Tailwind CSS
    Por Jeferson Huidobro el Gran Oso

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AGILE WEB</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Comenzamos con la web de Angelo Bendezu</h1>
+    <h1 class="text-sky-400 font-mono text-2xl">Comenzamos con la web de Angelo Bendezu</h1>
 </body>
 </html>
