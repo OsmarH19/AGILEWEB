277a9039a5b496e2bfd62d66e43a136c139bc2b6
OsmarH19 <115659833+OsmarH19@users.noreply.github.com>
2024-06-18 21:43:12 -0500

    Creando primera vista

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AGILE WEB</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <h1 class="text-sky-400 font-mono text-2xl">Comenzamos con la web de Angelo Bendezu</h1>
-</body>
-</html>
+<x-layout class="page-banner">
+    <x-slot:title> Osmar </x-slot:title>
+    <div style="background-color: red">
+        <div class="">
+            <h1 class="">EL GRAN OSO</h1>
+        </div>
+
+    </div>
+</x-layout>
